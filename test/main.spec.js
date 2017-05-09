2da3549c2227dbe8d4e25463fc709df2138516da
Fernando Navarro <fnp.formacion@gmail.com>
2017-05-09 22:33:51 +0200

    Implementar el ejemplo del plugin "simplebox" de https://github.com/kfern/tests con la estructura y stack de https://github.com/kkroening/karma-qunit-jquery

--- test/main.spec.js
+++ test/main.spec.js
@@ -8,28 +8,58 @@ QUnit.module('DOM manipulation', {
             .attr('id', 'fixture')
             .html(mainHtml[0].innerHTML);
         $(document.body).append(container);
-        onLoad();
     },
     afterEach: function() {
         $('#fixture').remove();
     }
 });
 
-QUnit.test("getName", function(assert) {
-    $('#name').val('Test');
-    assert.equal(getName(), 'Test');
+QUnit.test("Versión 1.0.0", function(assert) {
+    assert.equal($.fn.kfern.getVersion(), '1.0.0', 'La versión de los tests y del plugin coinciden.');    
 });
 
-QUnit.test("setOutput", function(assert) {
-    assert.equal('', $('#output').text());
-    setOutput('test');
-    assert.equal($('#output').text(), 'test');
+QUnit.test( 'Objetivo 1. Si el HTML no tiene la estructura mínima no se realiza ningún cambio.', function( assert ) {
+  // Arrange
+  var divtest = '.wrapper_incorrecto';
+  var start = $(divtest);
+  // Act
+  $(document).kfern.simplebox('simplebox');
+  // Assert (actual, expected)
+  assert.equal( $(divtest).length, 1, 'El contenido para los tests se ha cargado.' );
+  assert.deepEqual( $(divtest), start, 'Si el HTML no está completo no se realiza ningún cambio.' );
 });
 
-QUnit.test("click submit", function(assert) {
-    assert.equal('', $('#output').text());
-    $('#name').val('Test');
-    $('#submit').trigger('click');
-    assert.equal($('#output').text(), 'Hello, Test');
-});
+  QUnit.test( 'Objetivo 2. Se carga una página con la estructura básica.', function( assert ) {
+    // Arrange
+    var filtro = 'div.wrapper_a > div.simplebox';
+    // Act
+    $(document).kfern.simplebox();
+    // Assert (actual, expected)
+    var $result = $( filtro );
+    assert.ok( $result.attr('id'), 'El div contenedor tiene un id.' );
+    assert.ok( $result.data('kfern') == 'simplebox', 'El div contenedor se identifica correctamente.' );
+    assert.ok( $(filtro + ' > h2.simplebox-title').is(":visible")    == true, 'El título está visible.' );
+    assert.ok( $(filtro + ' > h2.simplebox-title > a').length == 1, 'El título es un enlace.' );    
+    assert.ok( $(filtro + ' > div.simplebox-content').is(":visible") == false, 'El contenido está oculto por defecto.' );
+  });
+  
+  QUnit.test( 'Objetivo 3. Implementar el comportamiento del componente cuando se pulsa en el título.', function( assert ) {
+    // Arrange
+    var filtro = 'div.wrapper_a > div.simplebox';
+    var contenido_visible;
+    // Act
+    $(document).kfern.simplebox();
+    contenido_visible = $(filtro + ' > div.simplebox-content').is(":visible");
+    $(filtro + ' > h2.simplebox-title > a').trigger('click');
+    // Assert
+    assert.ok( contenido_visible == false && $(filtro + ' > div.simplebox-content').is(":visible") == true, 'Si un contenido está oculto, cuando se pulse en el título se ha de mostrar el contenido asociado.' );
+    
+    //Si un contenido está visible, cuando se pulse en el título se ha de ocultar el contenido asociado
+    // Arrange
+    contenido_visible = $(filtro + ' > div.simplebox-content').is(":visible");
+    //Act
+    $(filtro + ' > h2.simplebox-title > a').trigger('click');
+    // Assert
+    assert.ok( contenido_visible && $(filtro + ' > div.simplebox-content').is(":visible") == false, 'Si un contenido está visible, cuando se pulse en el título se ha de ocultar el contenido asociado.' );
 
+  });
